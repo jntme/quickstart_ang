8cad08a87974367e35c6d639d37189d7324b6a51
jntme <mail@jntme.ch>
2017-03-23 08:56:51 +0100

    add simple patient

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,43 @@
 import { Component } from '@angular/core';
 
+export class Patient {
+  pid: number;
+  name: string;
+  prename: string;
+  gender: string;
+}
+
 @Component({
   selector: 'my-app',
-  template: `<h1>Hello {{name}}</h1>`,
+  template: `
+  <h1>{{title}}</h1>
+  <br />
+  <h2>{{patient.prename}} {{patient.name}}</h2>
+  <div>
+    <label>Prename: </label> 
+    <input [(ngModel)]="patient.prename" placeholder="prename">
+  </div>
+  <div>
+    <label>Name: </label> 
+    <input [(ngModel)]="patient.name" placholder="name">
+  </div>
+  <div><label>PID: </label>{{patient.pid}}</div>
+  <div>
+    <label>Gender: </label> 
+    <input type="text" value={{patient.gender}}>
+  </div>
+  `,
 })
-export class AppComponent  { name = 'Angular'; }
+export class AppComponent {
+  title = "Patients";
+
+  patient: Patient = {
+    pid: 1,
+    name: "Brönnimann",
+    prename: "Elisabeth",
+    gender: "female"
+  };
+
+}
+
+
